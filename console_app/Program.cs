ccf7ce0568c041fc02fee205897dff6bb88e66b6
Jason Meridth <my@email.com>
2009-06-03 22:59:35 -0500

    add name validation with tests

---
+++ console_app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using console_app.library;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessName();
+        }
+
+        static void ProcessName()
+        {
+            Console.WriteLine("What's your name?");
+            try
+            {
+                var name = Console.ReadLine();
+                if(!name.Validate()) throw new InvalidNameFormatException("Invalid name format, pleast try again");
+                
+                Console.WriteLine("Hello, " + name);    
+            }
+            catch(InvalidNameFormatException infe)
+            {
+                Console.WriteLine(infe.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
